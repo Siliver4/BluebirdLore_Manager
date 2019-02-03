f73a1b695e03ae30eb6bcac16f9d5ebec3e9afd1
Alexandre Mailliu <alex.mailliu21@gmail.com>
2019-02-03 16:55:12 +0100

    Basic listing of lore elements - using mock data to replace temporarily the bdd - using 'materialize css framework'

---
+++ dist/loreSheet.js
@@ -0,0 +1,8 @@
+"use strict";
+var LoreSheet = (function () {
+    function LoreSheet() {
+    }
+    return LoreSheet;
+}());
+exports.LoreSheet = LoreSheet;
+//# sourceMappingURL=loreSheet.js.map
---
+++ dist/mock-loreSheets.js
@@ -0,0 +1,88 @@
+"use strict";
+exports.LORESHEETS = [
+    {
+        id: 1,
+        title: "Sylvanas la reine banshee",
+        author: "Alexandre 'Rokh' MAILLIU",
+        authorUpdate: [new Date(), new Date()],
+        dateCreation: new Date(),
+        dateLastUpdate: new Date(),
+        imageURL: "https://gamepedia.cursecdn.com/wowpedia/thumb/5/51/SylvanasCinematic.png/200px-SylvanasCinematic.png?version=25ce4c6892624c009109396fb0c25ab7",
+        content: "Dame Sylvanas Coursevent, appelée aussi parfois 'la Dame noire' et 'la Reine banshee', est le chef de guerre de la Horde et le chef suprême des Réprouvés, une des plus puissantes factions de mort-vivants sur Azeroth. Durant sa vie, Sylvanas était le général des forestiers de Lune d'Argent, dont la sagacité de meneuse et les prouesses martiales étaient sans égal. Durant la Troisième Guerre, elle défendit bravement Quel'Thalas contre l'invasion du Fléau menée par le chevalier de la mort Arthas Menethil. En fin de compte, cependant, Sylvanas tomba au combat. Plutôt que d'honorer le général des forestiers d'une mort rapide, Arthas lui arracha son âme et la transforma en banshee : un fourbe et vengeur agent du roi-liche nourri par la haine.",
+        category: "character",
+        keyWord: ["sylvanas", "banshee", "reine", "dame noire"]
+    },
+    {
+        id: 2,
+        title: "Réprouvé",
+        author: "Alexandre 'Rokh' MAILLIU",
+        authorUpdate: [new Date(), new Date()],
+        dateCreation: new Date(),
+        dateLastUpdate: new Date(),
+        imageURL: "https://gamepedia.cursecdn.com/wowpedia/thumb/b/b4/Nathanos_Blightcaller_TCG.jpg/230px-Nathanos_Blightcaller_TCG.jpg?version=60dd868c72ba7b769b8101e4cdb2c0fb",
+        content: "Les Réprouvés, ou simplement Morts-Vivants, forment une race et une faction de morts-vivants intelligents ayant brisé les liens qui les unissaient autrefois au Fléau et qui occupent désormais la majeure partie du royaume de Lordaeron, détruit pendant la Troisième Guerre par le chevalier de la mort Arthas. Les Réprouvés sont menés par la Reine Banshee Sylvanas Coursevent, ancienne générale des Forestiers de Quel'Thalas. Leur capitale est Fossoyeuse, une nécropole bâtie dans les catacombes royales sous les ruines de la Cité de Lordaeron. ",
+        category: "race",
+        keyWord: ["réprouvé", "horde"]
+    },
+    {
+        id: 3,
+        title: "Chevalier de la mort",
+        author: "Alexandre 'Rokh' MAILLIU",
+        authorUpdate: [new Date(), new Date()],
+        dateCreation: new Date(),
+        dateLastUpdate: new Date(),
+        imageURL: "https://gamepedia.cursecdn.com/wowpedia/thumb/5/5d/Death_Knight_Crest.png/260px-Death_Knight_Crest.png?version=7f6bec980495fe95a962a1398f4ba9b7",
+        content: "Les chevaliers de la mort, souvent abrégé DK, sont des combattants de mêlée qui manient la magie sombre et des armes forgées de runes.",
+        category: "class",
+        keyWord: ["chevalier de la mort", "dk"]
+    },
+    {
+        id: 4,
+        title: "Norfendre",
+        author: "Alexandre 'Rokh' MAILLIU",
+        authorUpdate: [new Date(), new Date()],
+        dateCreation: new Date(),
+        dateLastUpdate: new Date(),
+        imageURL: "https://gamepedia.cursecdn.com/fr_wowpedia/thumb/c/ce/WorldMap-Northrend.jpg/340px-WorldMap-Northrend.jpg?version=cbebe879d0430e85b3faff27816ed08f",
+        content: "Norfendre est le continent glacial situé au nord du monde d'Azeroth, à partir duquel s'est répandu le Fléau. Il s'agit également de l'emplacement de la citadelle de la Couronne de glace, le siège du maléfique roi-liche. Le continent a été ajouté dans World of Warcraft lors de l'extension World of Warcraft: Wrath of the Lich King. ",
+        category: "place",
+        keyWord: ["norfendre", "nord", "glace", "roi lich"]
+    },
+    {
+        id: 5,
+        title: "Jaina Portvaillant",
+        author: "Alexandre 'Rokh' MAILLIU",
+        authorUpdate: [new Date(), new Date()],
+        dateCreation: new Date(),
+        dateLastUpdate: new Date(),
+        imageURL: "https://gamepedia.cursecdn.com/wowpedia/thumb/9/93/Jaina_FX.jpg/300px-Jaina_FX.jpg?version=c6ab01579b36828c053187e99d57f24c",
+        content: "Dame Jaina Portvaillant est la plus puissante sorcière humaine vivante, et l'ancienne dirigeante du Kirin Tor de Dalaran et de la cité portuaire de Theramore. Elle est la fille du Grand amiral Daelin Portvaillant et la sœur de Derek Portvaillant.",
+        category: "character",
+        keyWord: ["jaina", "portvaillant", "humaine", "magicienne"]
+    },
+    {
+        id: 6,
+        title: "Alliance",
+        author: "Alexandre 'Rokh' MAILLIU",
+        authorUpdate: [new Date(), new Date()],
+        dateCreation: new Date(),
+        dateLastUpdate: new Date(),
+        imageURL: "https://gamepedia.cursecdn.com/fr_wowpedia/thumb/f/f7/AllianceCrest.png/260px-AllianceCrest.png?version=b2e768bc5852a76ee295427d1ea064ed",
+        content: "Les humains, les elfes de la nuit, les nains, les gnomes, les draeneï et les féroces worgens constituent l'illustre Alliance. Fières et nobles, courageuses et sages, ces races coopèrent pour préserver l'ordre sur Azeroth. L'honneur et les traditions sont au coeur de l'Alliance, et ses dirigeants se font les champions de la justice, de l'espoir, de la connaissance et de la foi. Lorsque règnent le chaos et l'incertitude, l'Alliance reste ferme dans sa détermination à apporter la lumière dans les plus sombres recoins du monde.",
+        category: "other",
+        keyWord: ["alliance"]
+    },
+    {
+        id: 7,
+        title: "Horde",
+        author: "Alexandre 'Rokh' MAILLIU",
+        authorUpdate: [new Date(), new Date()],
+        dateCreation: new Date(),
+        dateLastUpdate: new Date(),
+        imageURL: "https://gamepedia.cursecdn.com/fr_wowpedia/thumb/0/08/Horde_Crest.png/260px-Horde_Crest.png?version=805848ce569e0dc05925be9a9ba32805",
+        content: "La Horde est composée d'orcs, de Réprouvés, de taurens, de trolls, d'elfes de sang et, depuis peu, de gobelins. Incomprises et rejetées, ces races variées et puissantes s'efforcent de surmonter leurs différences et de s'unir pour préserver la liberté de leurs peuples et prospérer sur une terre qui leur est hostile. L'action et la force prennent le pas sur la diplomatie dans la Horde, dont les dirigeants s'imposent par les armes plutôt que par la politique. La brutalité de ses champions a cependant un objectif, celui de faire entendre la voix de ceux qui se battent pour survivre.",
+        category: "other",
+        keyWord: ["horde"]
+    }
+];
+//# sourceMappingURL=mock-loreSheets.js.map
